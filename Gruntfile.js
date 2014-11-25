70f155b85d2e4b2a54359a358b4cf9c4d826a8ab
Kevin Dew <kev@redbullet.co.uk>
2014-11-25 12:45:32 +0000

    Bunch more slides

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,9 +86,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 6969,
         open: true,
-        livereload: 35729,
+        livereload: 35730,
         // Change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
